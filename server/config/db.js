a7f9c59c36b7895ceb0bc421433e4a48f050ad2f
janet850305 <janet850305@gmail.com>
2024-07-15 05:06:44 +0800

    db change

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("petfeed", "root", "b03b02019", {
-  host: "localhost",
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_NAME
+) {
+  console.error("Missing database configuration. Please check your .env file.");
+  process.exit(1);
+}
+
+const sequelize = new Sequelize({
   dialect: "mysql",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = sequelize;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 const sequelize = require("./config/db");
+const { Sequelize } = require("sequelize");
 
-sequelize
-  .authenticate()
-  .then(() => {
+(async () => {
+  try {
+    await sequelize.authenticate();
     console.log("Database connected");
-    return sequelize.sync();
-  })
-  .then(() => {
+    await sequelize.sync(); // 同步數據庫模型
     console.log("Models synchronized");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    process.exit(1); // 如果無法連接數據庫，退出程序
+  }
+})();
 // 使用 CORS 中间件
 app.use(cors());
 
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// 錯誤處理中間件
+// 错误处理中间件
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
